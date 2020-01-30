d13e7df870140e67a83dbfc0f10d18681ac5613e
Brad Hanel (RIT Student) <bxh9261@ad.rit.edu>
2020-01-30 12:02:44 -0500

    + 1/30 notes and organization

---
+++ IGME-330/1-30/IIFE/src/textLIB.js
@@ -0,0 +1,20 @@
+(function () {
+"use strict";
+function greetify(fullName){
+		let greetings = ["Hello", "Hi", "Bonjour", "Guten tag", "Hola", "Shalom", "Salve"];
+		let randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+		return `${randomGreeting} ${fullName}`;
+}
+	
+function shoutify(fullName){
+		let exclamations = ["!", "!!", "!?!", "?!!", "@#$%!", "???", "!!!"];
+		let randomExclamation = exclamations[Math.floor(Math.random() * exclamations.length)];
+		return `Hey ${fullName} ${randomExclamation}`.toUpperCase();
+}
+    
+window["textLIB"] = {
+    greetify,
+    shoutify
+};
+    
+})();
