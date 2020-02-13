89e9c5df3500f39a87ed636ba52d9a2144ebfb88
Brad Hanel (RIT Student) <bxh9261@ad.rit.edu>
2020-02-13 12:17:56 -0500

    + ES6 Notes 2/13/2020

---
+++ IGME-330/February/ES6 2-13-2020/index.js
@@ -0,0 +1,32 @@
+import * as anythingIWant from "utils.js";
+
+	const appData = {
+		appHeadingText: "Greeter App",
+		appInstructionsHeading: "Instructions for this exercise",
+		appInstructionsText: "We are going to refactor the code in this example so that all the JS is placed in multiple ES6 modules, and has been moved to external files:"
+	};
+	
+	function init(){
+		document.querySelector("h1").innerHTML = appData.appHeadingText;
+		document.querySelector("#instructionsHeading").innerHTML = appData.appInstructionsHeading;
+		document.querySelector("#instructions").innerHTML = appData.appInstructionsText;
+		
+		document.querySelector("#greetButton").onclick = greetButtonClicked;
+		document.querySelector("#shoutButton").onclick = shoutButtonClicked;
+	}
+
+	function greetButtonClicked(){
+		let firstName = document.querySelector("#firstName").value;
+		let lastName = document.querySelector("#lastName").value;
+		output.innerHTML = anythingIWant.greetify( `${firstName} ${lastName}` );
+	}
+	
+	function shoutButtonClicked(){
+		let firstName = document.querySelector("#firstName").value;
+		let lastName = document.querySelector("#lastName").value;
+		output.innerHTML = anythingIWant.shoutify( `${firstName} ${lastName}` );
+	}
+	
+    export{init};
+
+	
---
+++ IGME-330/February/ES6 2-13-2020/loader.js
@@ -0,0 +1,5 @@
+//if running on ftp it will fail due to CORS policy because "Google Chrome is BITCHING!" - Tony Jefferson
+
+import * as eee from "main.js";
+
+window.onload = eee.init;
